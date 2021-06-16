89b4eede3a10084cf5b721dac772c2d6ccd7bd01
Erwin Meier <emeier.duguet@gmail.com>
2021-06-15 22:24:10 -0400

    Arreglando errores, ahora los dataframe de temperatura y precipitaciones tienen el mismo tamaño

--- csv/juntarscvpandas.py
+++ csv/juntarscvpandas.py
@@ -1,19 +1,19 @@
 import pandas as pd
 from collections import Counter
 
-temperaturas = pd.read_csv("/home/erwin/Desktop/ProyectoIngenieriaDatos/csv/temperaturas.csv",
+temperaturas = pd.read_csv("csv/temperaturas.csv",
     names = ['_id','IdEstacion','Nombre','Latitud','Longitud','Altura','AA+-o','Mes',
     'Dia','TMinima','TMaxima'])
-precipitaciones = pd.read_csv("/home/erwin/Desktop/ProyectoIngenieriaDatos/csv/precipitaciones.csv",
+precipitaciones = pd.read_csv("csv/precipitaciones.csv",
     names = ['_id','IdEstacion','Nombre','Latitud','Longitud','Altura','AA+-o','Mes',
     'Dia','Valor'])
 
 
 temperaturas.drop(index=temperaturas.index[0], axis=0, inplace=True)
 precipitaciones.drop(index=precipitaciones.index[0], axis=0, inplace=True)
-print(temperaturas[['_id','IdEstacion','Nombre','Latitud','Longitud',
-    'AA+-o','Mes','Dia','TMinima','TMaxima']])
-print(precipitaciones[['Valor']])
+#print(temperaturas[['_id','IdEstacion','Nombre','Latitud','Longitud',
+#    'AA+-o','Mes','Dia','TMinima','TMaxima']])
+#print(precipitaciones[['Valor']])
 
 countTemp = list(Counter(temperaturas['Nombre']).keys())
 
@@ -26,6 +26,8 @@ else:
 # Crear dos listas distintas para ver que estaciones tienen 366 datos en ambas dataframe
 listTemp = list(Counter(temperaturas['Nombre']).values())
 listPrec = list(Counter(precipitaciones['Nombre']).values())
+print(listTemp)
+print(listPrec)
 
 # Crear lista vacia para agregar nombres en ambas listas con 366 datos
 mezcla = [countTemp,listTemp,[]]
@@ -33,21 +35,25 @@ for i in range(len(mezcla[1])):
     mezcla[2].append("Distintos")
 
 for i in range(len(mezcla[1])):
-    for e in range(len(listPrec)):
-        if(mezcla[1][i]==listPrec[e]):
+    if(mezcla[1][i]==listPrec[i]):
+        print(f'{mezcla[1][i]}==>{listPrec[i]}')
+        if(listPrec[i]==366):
             mezcla[2][i]="Iguales"
 
-# for i in mezcla[1]:
-#     for e in listPrec:
-#         if(i==e):
-#             mezcla[2].append("iguales")
-#         else:
-#             mezcla[2].append("distintos")   
 
-for i in range(len(mezcla)):
+for i in range(len(mezcla[1])):
     if(mezcla[2][i]=="Distintos"):
-        t#emperaturas.drop(temperaturas[temperaturas.Nombre == mezcla[0][i]].index, inplace=True)
-        temperaturas = temperaturas[temperaturas.Nombre == mezcla[0][i]]
+        print(mezcla[0][i])
+        temperaturas.drop(temperaturas[temperaturas.Nombre == mezcla[0][i]].index, inplace=True)
+        precipitaciones.drop(precipitaciones[precipitaciones.Nombre == mezcla[0][i]].index, inplace=True)
 
+print(temperaturas)     
+#print(Counter(temperaturas['Nombre']).values())   
+#print(Counter(temperaturas['Nombre']).keys())   
 
-print(temperaturas)        
+# # Ver la cantidad de filas unicas en la dataframe
+# #print(temperaturas.nunique())
+
+print(precipitaciones)  
+#print(Counter(precipitaciones['Nombre']).values())   
+print(Counter(precipitaciones['Nombre']).keys())     
