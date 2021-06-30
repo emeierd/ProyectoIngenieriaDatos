6cb024618dad6010251372a9ebcf618cee16b98c
Erwin Meier <emeier.duguet@gmail.com>
2021-06-30 19:42:14 -0400

    Testing para comparacion

---
+++ test.py
@@ -0,0 +1,66 @@
+import pandas as pd
+from collections import Counter
+import pyodbc
+from datetime import datetime, timedelta
+
+hoy = datetime.now()
+ayer=hoy-timedelta(hours=24)
+format ='%Y-%m-%d'
+ayer = ayer.strftime(format)
+
+data = pd.read_csv("csv/data.csv",
+    names = ['Nombre','Coordenadas','Fecha','TMinima','TMaxima','Precipitaciones'])
+
+data.drop(index=data.index[0], axis=0, inplace=True) 
+
+conn = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};"
+                      "Server=192.168.1.64,1433;"
+                      "Database=ProjectoID;"
+                      "uid=sa;"
+                      "pwd=aselga123")
+
+#@app.route('/tiempo/comparar/<id>')
+def comparar(id):
+    hoy = datetime.now()
+    ayer=hoy-timedelta(hours=24)
+    format ='%Y-%m-%d'
+    ayer = ayer.strftime(format)
+
+    # data = pd.read_csv("/home/erwin/Desktop/ProyectoIngenieriaDatos/csv/data.csv",
+    #     names = ['Nombre','Coordenadas','Fecha','TMinima','TMaxima','Precipitaciones'])
+    data = pd.read_csv("csv/data.csv",
+        names = ['Nombre','Coordenadas','Fecha','TMinima','TMaxima','Precipitaciones'])
+
+    data.drop(index=data.index[0], axis=0, inplace=True) 
+
+    nombre = id.replace("_"," ")
+    nombrebd = id+"_24"
+    data = data[data.Nombre == nombre]
+
+    query = f"SELECT * FROM {nombrebd} WHERE fecha LIKE '{ayer}%'"
+
+    cursor = conn.cursor()
+    cursor.execute(query)
+    resumen = cursor.fetchall()
+    fechabd = resumen[0][2]
+    fechabd = fechabd.split("-")[1]+"-"+fechabd.split("-")[2]
+
+    for index, row in data.iterrows():
+        fecha = row['Fecha']
+        split = fecha.split("-")
+        mes = split[1]
+        dia = split[2]
+        if (len(mes)==1):
+            mes = "0"+mes
+        if (len(dia)==1):
+            dia = "0"+dia    
+
+        fecharow = mes+"-"+dia    
+        if (fechabd == fecharow):
+            print(f"temperatura minima: 2012: {row['TMinima']} 2021: {resumen[0][3]}")
+            print(f"temperatura maxima: 2012: {row['TMaxima']} 2021: {resumen[0][4]}")
+            print(f"total precipitaciones: 2012: {row['Precipitaciones']} 2021: {resumen[0][5]}")
+
+
+
+comparar("Temuco")
